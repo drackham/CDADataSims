c9e040ea59acfb64f17a12ceb808a3b2ea68f630
drackham <drackham@gmail.com>
2016-01-13 22:48:52 -0700

    Added all item parameters and corresponding tests.

--- R/hartzRoussosData.R
+++ R/hartzRoussosData.R
@@ -3,7 +3,7 @@
 #' Simulates the Hartz Roussos data from "The Fusion Model for Skills Diagnosis: Blending Theory with Practicality" (2008)
 #' @param JJ Number of examinees
 #' @param j Examinee j
-#' @param II Number of ittems
+#' @param II Number of items
 #' @param i Item i
 #' @param KK Number of skills
 #' @param k Skill k
@@ -11,8 +11,9 @@
 #' @param alphaJK Examinee skill mastery profile
 #' @param x response matrix
 #' @param pi Probability that an examinee having mastered all the Q required skills for item i will correctly apply all the skills when solving item i.
-#' @param r Item discrimination
-#' @param c Item difficulty
+#' @param iParamsLow Matrix of item parameters for ideal low complexity model
+#' @param r Item discrimination r
+#' @param c Item difficulty c
 #' @keywords hartz roussos
 #' @export
 #' @examples
@@ -22,9 +23,11 @@ hartzRoussosData <- function(){
   set.seed(314159)
 
   JJ <- 1500
+  II <- 40
   KK <- 7
   alphaK <- c(.3, .4, .45, .5, .55, .6, .65)
-  alphaJK <- matrix(nrow=JJ, ncol=KK)
+  alphaJK <- matrix(nrow = JJ, ncol = KK)
+  iParamsLow <- matrix(nrow = II, ncol = KK + 2) # n skills k + 1 r param and + 1 c param
 
   for (j in 1:JJ){
     for (k in 1:KK){
@@ -32,7 +35,60 @@ hartzRoussosData <- function(){
     }
   }
 
+  # Note 1/13/16: iParamsLow[4,9], [5,25], [6,13] and [6,20] are incorrectly specified in
+  # The Fusion Model for Skills Diagnosis: Blending Theory with Practicality" (2008). While
+  # values are provided, QLow is 0 for those cells. To ensure the unit tests passed I multiplied
+  # the misspecified values by 0 so the link would remain clear.
 
-  out <- list("alphaJK" = alphaJK)
+                      # pi   r1    r2    r3    r4     r5   r6    r7     c
+  iParamsLow[1,] <-  c(.869, NA  , NA  , .447, NA  , .197, NA  , NA  , 1.128)
+  iParamsLow[2,] <-  c(.834, .146, NA  , NA  , NA  , NA  , NA  , NA  , .156)
+  iParamsLow[3,] <-  c(.936, .158, NA  , NA  , NA  , .122, NA  , NA  , 1.796)
+  iParamsLow[4,] <-  c(.896, NA  , NA  , NA  , NA  , NA  , NA  , .13 , 1.295)
+  iParamsLow[5,] <-  c(.957, .177, NA  , NA  , .157, NA  , NA  , NA  , 2.06)
+  iParamsLow[6,] <-  c(.889, NA  , .494, NA  , NA  , .442, NA  , .184, 2.476)
+  iParamsLow[7,] <-  c(.827, NA  , NA  , .403, .405, NA  , NA  , .111, 1.951)
+  iParamsLow[8,] <-  c(.805, NA  , NA  , .464, NA  , .132, NA  , NA  , .647)
+  iParamsLow[9,] <-  c(.894, NA  , NA  , .493, NA  , .171, NA  , NA  , .45)
+  iParamsLow[10,] <- c(.871, NA  , NA  , NA  , .153, NA  , NA  , NA  , .684)
+
+                      # pi   r1    r2    r3    r4     r5   r6    r7     c
+  iParamsLow[11,] <-  c(.861, NA  , NA  , NA  , NA  , NA  , .118, NA  , .281)
+  iParamsLow[12,] <-  c(.907, NA  , NA  , NA  , NA  , NA  , NA  , .104, .642)
+  iParamsLow[13,] <-  c(.953, .575, NA  , NA  , .167, NA  , NA  , .14 , 1.872)
+  iParamsLow[14,] <-  c(.838, NA  , NA  , NA  , .105, NA  , NA  , NA  , 2.164)
+  iParamsLow[15,] <-  c(.965, NA  , .114, .197, NA  , NA  , NA  , NA  , .093)
+  iParamsLow[16,] <-  c(.884, NA  , NA  , NA  , NA  , .104, NA  , .198, .686)
+  iParamsLow[17,] <-  c(.831, NA  , NA  , NA  , NA  , .477, NA  , .153, .633)
+  iParamsLow[18,] <-  c(.952, .142, NA  , .43 , NA  , NA  , .103, .162, 2.406)
+  iParamsLow[19,] <-  c(.928, .516, NA  , NA  , NA  , NA  , .179, NA  , .171)
+  iParamsLow[20,] <-  c(.962, .509, NA  , NA  , NA  , NA  , NA  , NA  , 1.371)
+
+                      # pi   r1    r2    r3    r4     r5   r6    r7     c
+  iParamsLow[21,] <-  c(.921, NA  , .18 , NA  , NA  , NA  , NA  , NA  , 1.671)
+  iParamsLow[22,] <-  c(.911, NA  , NA  , NA  , NA  , NA  , .191, NA  , .057)
+  iParamsLow[23,] <-  c(.856, NA  , NA  , NA  , .192, NA  , NA  , NA  , .267)
+  iParamsLow[24,] <-  c(.833, NA  , NA  , NA  , .415, NA  , .183, NA  , 1.709)
+  iParamsLow[25,] <-  c(.877, NA  , .115, NA  , NA  , NA  , .143, NA  , 1.67)
+  iParamsLow[26,] <-  c(.934, NA  , NA  , NA  , NA  , .148, NA  , NA  , 1.755)
+  iParamsLow[27,] <-  c(.939, NA  , NA  , NA  , NA  , NA  , .495, .185, 2.331)
+  iParamsLow[28,] <-  c(.928, NA  , .445, .183, .195, NA  , NA  , NA  , 1.013)
+  iParamsLow[29,] <-  c(.903, NA  , .402, .496, NA  , .104, NA  , .181, 1.006)
+  iParamsLow[30,] <-  c(.958, NA  , NA  , .419, NA  , NA  , NA  , .159, 1.429)
+
+                      # pi   r1    r2    r3    r4     r5   r6    r7     c
+  iParamsLow[31,] <-  c(.897, .191, NA  , NA  , .14 , NA  , .187, NA  , 1.831)
+  iParamsLow[32,] <-  c(.801, NA  , .455, NA  , .186, NA  , NA  , NA  , 1.819)
+  iParamsLow[33,] <-  c(.907, NA  , NA  , NA  , .157, NA  , NA  , NA  , .593)
+  iParamsLow[34,] <-  c(.84 , NA  , .141, .404, NA  , NA  , .168, NA  , 1.108)
+  iParamsLow[35,] <-  c(.88 , .598, .127, .168, NA  , NA  , NA  , NA  , 2.336)
+  iParamsLow[36,] <-  c(.936, NA  , .46 , NA  , NA  , .149, NA  , NA  , 2.258)
+  iParamsLow[37,] <-  c(.809, .501, NA  , NA  , .175, NA  , NA  , NA  , 2.43)
+  iParamsLow[38,] <-  c(.866, NA  , NA  , NA  , NA  , .434, .167, NA  , .172)
+  iParamsLow[39,] <-  c(.826, .13 , NA  , NA  , NA  , NA  , .105, .13 , 2.329)
+  iParamsLow[40,] <-  c(.868, NA  , NA  , NA  , NA  , NA  , .19, .186 , 1.356)
+
+
+  out <- list("alphaJK" = alphaJK, "iParamsLow" = iParamsLow)
   return(out)
 }
--- tests/testthat/testHartzRoussosData.R
+++ tests/testthat/testHartzRoussosData.R
@@ -1,6 +1,7 @@
 context("Check the Hartz Roussos data simulation for correctness")
 
 data <- hartzRoussosData()
+q <- hartzRoussosQLow()
 
 test_that("alphaJK was simulated", {
   expect_that(data$alphaJK, is_a("matrix"))
@@ -11,7 +12,7 @@ test_that("alphaJK has the proper dimensions", {
   expect_that(ncol(data$alphaJK), equals(7))
 })
 
-test_that("alphaJK hascorrect proportions of mastery", {
+test_that("alphaJK has correct proportions of mastery", {
   expect_that(mean(data$alphaJK[,1]), equals(.3, tolerance = 0.1))
   expect_that(mean(data$alphaJK[,2]), equals(.4, tolerance = 0.1))
   expect_that(mean(data$alphaJK[,3]), equals(.45, tolerance = 0.1))
@@ -19,4 +20,88 @@ test_that("alphaJK hascorrect proportions of mastery", {
   expect_that(mean(data$alphaJK[,5]), equals(.55, tolerance = 0.1))
   expect_that(mean(data$alphaJK[,6]), equals(.6, tolerance = 0.1))
   expect_that(mean(data$alphaJK[,7]), equals(.65, tolerance = 0.1))
+})
+
+test_that("iParamsLow has proper dimensions", {
+  expect_that(nrow(data$iParamsLow), equals(40))
+  expect_that(ncol(data$iParamsLow), equals(9)) # 1 pi 7 r and 1 c = 9
+})
+
+test_that("iParamsLow rows are correctly specified", {
+  # Note 1/13/16: iParamsLow[4,9], [5,25], [6,13] and [6,20] are incorrectly specified in
+  # The Fusion Model for Skills Diagnosis: Blending Theory with Practicality" (2008).
+
+  expect_that(sum(data$iParamsLow[1,], na.rm=TRUE), equals(2.641))
+  expect_that(sum(data$iParamsLow[2,], na.rm=TRUE), equals(1.136))
+  expect_that(sum(data$iParamsLow[3,], na.rm=TRUE), equals(3.012))
+  expect_that(sum(data$iParamsLow[4,], na.rm=TRUE), equals(2.321))
+  expect_that(sum(data$iParamsLow[5,], na.rm=TRUE), equals(3.351))
+  expect_that(sum(data$iParamsLow[6,], na.rm=TRUE), equals(4.485))
+  expect_that(sum(data$iParamsLow[7,], na.rm=TRUE), equals(3.697))
+  expect_that(sum(data$iParamsLow[8,], na.rm=TRUE), equals(2.048))
+  expect_that(sum(data$iParamsLow[9,], na.rm=TRUE), equals(2.008)) # modified
+  expect_that(sum(data$iParamsLow[10,], na.rm=TRUE), equals(1.708))
+
+  expect_that(sum(data$iParamsLow[11,], na.rm=TRUE), equals(1.26))
+  expect_that(sum(data$iParamsLow[12,], na.rm=TRUE), equals(1.653))
+  expect_that(sum(data$iParamsLow[13,], na.rm=TRUE), equals(3.707)) # modified
+  expect_that(sum(data$iParamsLow[14,], na.rm=TRUE), equals(3.107))
+  expect_that(sum(data$iParamsLow[15,], na.rm=TRUE), equals(1.369))
+  expect_that(sum(data$iParamsLow[16,], na.rm=TRUE), equals(1.872))
+  expect_that(sum(data$iParamsLow[17,], na.rm=TRUE), equals(2.094))
+  expect_that(sum(data$iParamsLow[18,], na.rm=TRUE), equals(4.195))
+  expect_that(sum(data$iParamsLow[19,], na.rm=TRUE), equals(1.794))
+  expect_that(sum(data$iParamsLow[20,], na.rm=TRUE), equals(2.842)) # modified
+
+  expect_that(sum(data$iParamsLow[21,], na.rm=TRUE), equals(2.772))
+  expect_that(sum(data$iParamsLow[22,], na.rm=TRUE), equals(1.159))
+  expect_that(sum(data$iParamsLow[23,], na.rm=TRUE), equals(1.315))
+  expect_that(sum(data$iParamsLow[24,], na.rm=TRUE), equals(3.14))
+  expect_that(sum(data$iParamsLow[25,], na.rm=TRUE), equals(2.805)) # modified
+  expect_that(sum(data$iParamsLow[26,], na.rm=TRUE), equals(2.837))
+  expect_that(sum(data$iParamsLow[27,], na.rm=TRUE), equals(3.95))
+  expect_that(sum(data$iParamsLow[28,], na.rm=TRUE), equals(2.764))
+  expect_that(sum(data$iParamsLow[29,], na.rm=TRUE), equals(3.092))
+  expect_that(sum(data$iParamsLow[30,], na.rm=TRUE), equals(2.965))
+
+  expect_that(sum(data$iParamsLow[31,], na.rm=TRUE), equals(3.246))
+  expect_that(sum(data$iParamsLow[32,], na.rm=TRUE), equals(3.261))
+  expect_that(sum(data$iParamsLow[33,], na.rm=TRUE), equals(1.657))
+  expect_that(sum(data$iParamsLow[34,], na.rm=TRUE), equals(2.661))
+  expect_that(sum(data$iParamsLow[35,], na.rm=TRUE), equals(4.109))
+  expect_that(sum(data$iParamsLow[36,], na.rm=TRUE), equals(3.803))
+  expect_that(sum(data$iParamsLow[37,], na.rm=TRUE), equals(3.915))
+  expect_that(sum(data$iParamsLow[38,], na.rm=TRUE), equals(1.639))
+  expect_that(sum(data$iParamsLow[39,], na.rm=TRUE), equals(3.52))
+  expect_that(sum(data$iParamsLow[40,], na.rm=TRUE), equals(2.60))
+
+})
+
+test_that("iParamsLow columns are correctly specified", {
+  expect_that(sum(data$iParamsLow[,1], na.rm=TRUE), equals(35.549))
+  expect_that(sum(data$iParamsLow[,2], na.rm=TRUE), equals(3.643))
+  expect_that(sum(data$iParamsLow[,3], na.rm=TRUE), equals(2.933))
+  expect_that(sum(data$iParamsLow[,4], na.rm=TRUE), equals(4.104))
+  expect_that(sum(data$iParamsLow[,5], na.rm=TRUE), equals(2.447)) # modified
+  expect_that(sum(data$iParamsLow[,6], na.rm=TRUE), equals(2.48)) # modified
+  expect_that(sum(data$iParamsLow[,7], na.rm=TRUE), equals(2.229)) # modified
+  expect_that(sum(data$iParamsLow[,8], na.rm=TRUE), equals(2.023))
+  expect_that(sum(data$iParamsLow[,9], na.rm=TRUE), equals(52.102))
+})
+
+test_that("QLow and iParamsLow are congruent", {
+
+  # If iParamsLow is correctly specified then multiplying the component vectors of
+  # iParamsLow[,2:8] * Q should give the same marginal column sum
+
+  expect_that(nrow(q*data$iParamsLow[,c(2:8)]),equals(40))
+  expect_that(ncol(q*data$iParamsLow[,c(2:8)]),equals(7))
+
+  expect_that(sum(q[,1]*data$iParamsLow[,2], na.rm=TRUE), equals(3.643))
+  expect_that(sum(q[,2]*data$iParamsLow[,3], na.rm=TRUE), equals(2.933))
+  expect_that(sum(q[,3]*data$iParamsLow[,4], na.rm=TRUE), equals(4.104))
+  expect_that(sum(q[,4]*data$iParamsLow[,5], na.rm=TRUE), equals(2.447)) # modified
+  expect_that(sum(q[,5]*data$iParamsLow[,6], na.rm=TRUE), equals(2.48)) # modified
+  expect_that(sum(q[,6]*data$iParamsLow[,7], na.rm=TRUE), equals(2.229)) # modified
+  expect_that(sum(q[,7]*data$iParamsLow[,8], na.rm=TRUE), equals(2.023))
 })
