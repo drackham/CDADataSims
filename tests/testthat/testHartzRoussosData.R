748900c1d1aa92f1d1980db93c4f2ed798d08c1b
drackham <drackham@gmail.com>
2016-01-26 23:01:52 -0700

    Improved handling of the tolerance equating by including a scaling factor of 1 since R does this in a very non-intuitive way.

--- tests/testthat/testHartzRoussosData.R
+++ tests/testthat/testHartzRoussosData.R
@@ -16,13 +16,13 @@ test_that("alphaJK has the proper dimensions", {
 })
 
 test_that("alphaJK has correct proportions of mastery", {
-  expect_that(mean(data$alphaJK[,1]), equals(.3, tolerance = 0.1))
-  expect_that(mean(data$alphaJK[,2]), equals(.4, tolerance = 0.1))
-  expect_that(mean(data$alphaJK[,3]), equals(.45, tolerance = 0.1))
-  expect_that(mean(data$alphaJK[,4]), equals(.5, tolerance = 0.1))
-  expect_that(mean(data$alphaJK[,5]), equals(.55, tolerance = 0.1))
-  expect_that(mean(data$alphaJK[,6]), equals(.6, tolerance = 0.1))
-  expect_that(mean(data$alphaJK[,7]), equals(.65, tolerance = 0.1))
+  expect_that(mean(data$alphaJK[,1]), equals(.3, tolerance = 0.1, scale=1))
+  expect_that(mean(data$alphaJK[,2]), equals(.4, tolerance = 0.1, scale=1))
+  expect_that(mean(data$alphaJK[,3]), equals(.45, tolerance = 0.1, scale=1))
+  expect_that(mean(data$alphaJK[,4]), equals(.5, tolerance = 0.1, scale=1))
+  expect_that(mean(data$alphaJK[,5]), equals(.55, tolerance = 0.1, scale=1))
+  expect_that(mean(data$alphaJK[,6]), equals(.6, tolerance = 0.1, scale=1))
+  expect_that(mean(data$alphaJK[,7]), equals(.65, tolerance = 0.1, scale=1))
 })
 
 test_that("iParamsLow has proper dimensions", {
