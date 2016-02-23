e569a404b5237e22a420811cedcb8d0f856447b4
drackham <drackham@gmail.com>
2016-02-22 20:47:01 -0700

    Updates to everything. Specifically fixed the subscripting issue.

--- R/simplifiedRumData.R
+++ R/simplifiedRumData.R
@@ -2,54 +2,50 @@
 #'
 #' Creates response data for a simplified version of the RUM model using the Hartz Roussos Q matrix (Low)
 #'
-#' @param JJ Number of examinees
-#' @param j Examinee j
-#' @param II Number of items
-#' @param i Item i
-#' @param KK Number of skills
-#' @param k Skill k
+#' @param J Number of examinees
+#' @param jj Student for observation n
+#' @param I Number of items
+#' @param ii Item for observation n
+#' @param K Number of skills
 #' @param alphaK Skill mastery population proportion vector
 #' @param alphaJK Examinee skill mastery profile
 #' @param masteryJK Continuous probability of mastery
 #' @param x response matrix
-#' @param pi Probability that an examinee having mastered all the Q required skills for item i will correctly apply all the skills when solving item i.
 #' @param iParamsLow Matrix of item parameters for ideal low complexity model
-#' @param r Item discrimination r
-#' @param c Item difficulty c
-#' @param eta Latent ability NOT included in the q-matrix
+#' @param rStar Item discrimination r
 #' @param kappa Mastery threshold parameter
+#'
 #' @author Dave Rackham \email{ddrackham@gmail.com}
 #' @references \url{http://onlinelibrary.wiley.com/doi/10.1002/j.2333-8504.2008.tb02157.x/abstract}
 #' @keywords q-matrix hartz roussos
 #'
 #' @examples
-#' data <- simplifiedRUM()
+#' data <- simplifiedRUMData()
 #'
 #' @export
-#'
-simplifiedRUM <- function(){
+
+simplifiedRUMData <- function(){
   set.seed(314159)
 
   kappa <- .7
-  JJ <- 1500
-  II <- 40
-  KK <- 7
+  J <- 1500
+  I <- 40
+  K <- 7
   q <- hartzRoussosQLow()
 
-  # Generate the final mastery values
+  # Generate the final mastery proportions
   alphaK <- c(.3, .4, .45, .5, .55, .6, .65)
-  alphaJK <- matrix(nrow = JJ, ncol = KK)
-
-  for (j in 1:JJ){
-    for (k in 1:KK){
+  alphaJK <- matrix(nrow = J, ncol = K)
+  for (j in 1:J){
+    for (k in 1:K){
       alphaJK[j,k] <- rbinom(1,1,alphaK[k])
     }
   }
 
   # Use alphaJK to randomly generate continuous probablity of mastery
-  masteryJK <- matrix(nrow=JJ, ncol=KK)
-  for (j in 1:JJ){
-    for(k in 1:KK){
+  masteryJK <- matrix(nrow=J, ncol=K)
+  for (j in 1:J){
+    for(k in 1:K){
       if(alphaJK[j,k] == 1){
         masteryJK[j,k] <- round(runif(1, min = kappa, max = 1),3)
       }
@@ -59,9 +55,9 @@ simplifiedRUM <- function(){
     }
   }
 
-  iParamsLow <- matrix(nrow = II, ncol = KK) # n skills
+  iParamsLow <- matrix(nrow = I, ncol = K) # n skills
 
-                      # r1    r2    r3    r4     r5   r6    r7
+                        # r1    r2    r3    r4     r5   r6    r7
   iParamsLow[1,] <-  c( NA  , NA  , .447, NA  , .197, NA  , NA  )
   iParamsLow[2,] <-  c( .146, NA  , NA  , NA  , NA  , NA  , NA  )
   iParamsLow[3,] <-  c( .158, NA  , NA  , NA  , .122, NA  , NA  )
@@ -73,7 +69,7 @@ simplifiedRUM <- function(){
   iParamsLow[9,] <-  c( NA  , NA  , .493, NA  , .171, NA  , NA  )
   iParamsLow[10,] <- c( NA  , NA  , NA  , .153, NA  , NA  , NA  )
 
-  # pi*   r1    r2    r3    r4     r5   r6    r7     c
+                        # r1    r2    r3    r4     r5   r6    r7
   iParamsLow[11,] <-  c( NA  , NA  , NA  , NA  , NA  , .118, NA  )
   iParamsLow[12,] <-  c( NA  , NA  , NA  , NA  , NA  , NA  , .104)
   iParamsLow[13,] <-  c( .575, NA  , NA  , .167, NA  , NA  , .14 )
@@ -85,7 +81,7 @@ simplifiedRUM <- function(){
   iParamsLow[19,] <-  c( .516, NA  , NA  , NA  , NA  , .179, NA  )
   iParamsLow[20,] <-  c( .509, NA  , NA  , NA  , NA  , NA  , NA  )
 
-  # pi*   r1    r2    r3    r4     r5   r6    r7     c
+                        # r1    r2    r3    r4     r5   r6    r7
   iParamsLow[21,] <-  c( NA  , .18 , NA  , NA  , NA  , NA  , NA  )
   iParamsLow[22,] <-  c( NA  , NA  , NA  , NA  , NA  , .191, NA  )
   iParamsLow[23,] <-  c( NA  , NA  , NA  , .192, NA  , NA  , NA  )
@@ -97,7 +93,7 @@ simplifiedRUM <- function(){
   iParamsLow[29,] <-  c( NA  , .402, .496, NA  , .104, NA  , .181)
   iParamsLow[30,] <-  c( NA  , NA  , .419, NA  , NA  , NA  , .159)
 
-  # pi*   r1    r2    r3    r4     r5   r6    r7     c
+                        # r1    r2    r3    r4     r5   r6    r7
   iParamsLow[31,] <-  c( .191, NA  , NA  , .14 , NA  , .187, NA  )
   iParamsLow[32,] <-  c( NA  , .455, NA  , .186, NA  , NA  , NA  )
   iParamsLow[33,] <-  c( NA  , NA  , NA  , .157, NA  , NA  , NA  )
@@ -110,23 +106,46 @@ simplifiedRUM <- function(){
   iParamsLow[40,] <-  c( NA  , NA  , NA  , NA  , NA  , .19, .186 )
 
 
-  probCorrect <- matrix (nrow=JJ, ncol=II)
-  for (j in 1:JJ){ # respondents
-    for (i in 1:II){ # items
+  probCorrect <- matrix (nrow=I, ncol=J)
+  for (i in 1:I){ # respondents
+    for (j in 1:J){ # items
       rVec <- iParamsLow[i,]
       rStar <- rVec^((1-alphaJK[j,])*q[i,])
-      probCorrect[j,i] <- prod(rStar)
+      probCorrect[i,j] <- round(prod(rStar),3)
 
     }
   }
 
-  y <- matrix (nrow=JJ, ncol=II)
-  for (j in 1:JJ){
-    for (i in 1:II){
-      y[j,i] <- rbinom(1,1,probCorrect[j,i])
+  # Generate response matrix
+  xMat <- matrix (nrow=I, ncol=J)
+  for (i in 1:I){
+    for (j in 1:J){
+      xMat[i,j] <- rbinom(1,1,probCorrect[i,j])
     }
   }
 
-  out <- list()
+  # Create vectorized response data
+  observed <- matrix(1,nrow=I,ncol=J)
+  N <- sum(observed)
+  # qVec <- rep(-1, N*K)
+  x <- rep(-1,N)
+  jj <- rep(-1,N)
+  ii <- rep(-1,N)
+  n <- 1
+  for (i in 1:I) {
+    for (j in 1:J) {
+      if (observed[i,j]) {
+        x[n] <- xMat[i,j]
+        jj[n] <- j
+        ii[n] <- i
+        # qVec[n] <- q[j,]
+        n <- n + 1
+      }
+    }
+  }
+
+  out <- list("I" = I, "J" = J, "K" = K, "N" = N, "xMat" = xMat, "x" = x, "jj" = jj, "ii" = ii,
+              "probCorrect" = probCorrect, "alphaJK" = alphaJK, "kappa" = kappa, "iParamsLow" = iParamsLow)
+
   return(out)
 }
---
+++ tests/testthat/testSimplifiedRumData.R
@@ -0,0 +1,45 @@
+context("Check the Simplified RUM data simulation for correctness")
+
+data <- simplifiedRUMData()
+q <- hartzRoussosQLow()
+J <- 1500
+I <- 40
+K <- 7
+
+test_that("alphaJK has the proper dimensions", {
+  expect_that(nrow(data$alphaJK), equals(J))
+  expect_that(ncol(data$alphaJK), equals(K))
+})
+
+test_that("alphaJK has correct proportions of mastery", {
+  expect_that(mean(data$alphaJK[,1]), equals(.3, tolerance = 0.1, scale=1))
+  expect_that(mean(data$alphaJK[,2]), equals(.4, tolerance = 0.1, scale=1))
+  expect_that(mean(data$alphaJK[,3]), equals(.45, tolerance = 0.1, scale=1))
+  expect_that(mean(data$alphaJK[,4]), equals(.5, tolerance = 0.1, scale=1))
+  expect_that(mean(data$alphaJK[,5]), equals(.55, tolerance = 0.1, scale=1))
+  expect_that(mean(data$alphaJK[,6]), equals(.6, tolerance = 0.1, scale=1))
+  expect_that(mean(data$alphaJK[,7]), equals(.65, tolerance = 0.1, scale=1))
+})
+
+test_that("iParamsLow has proper dimensions", {
+  expect_that(nrow(data$iParamsLow), equals(I))
+  expect_that(ncol(data$iParamsLow), equals(K)) # 7 r*
+})
+
+
+test_that("xMat is correctly formatted", {
+  expect_that(nrow(data$xMat), equals(I))
+  expect_that(ncol(data$xMat), equals(J))
+})
+
+test_that("N is the correct length", {
+  expect_that(data$N, equals(J*I))
+})
+
+test_that("jj is correctly formatted", {
+  expect_that(length(data$jj), equals(J*I))
+})
+
+test_that("ii is correctly formatted", {
+  expect_that(length(data$ii), equals(J*I))
+})
